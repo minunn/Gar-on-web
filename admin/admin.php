750cecb9f5baf508f318a0e1b9933e041c8f1f3d
GuilhemDeMarco <55885506+GuilhemDeMarco@users.noreply.github.com>
2020-05-06 13:58:21 +0200

    correction bug où la map s'affichait pas

--- admin/admin.php
+++ admin/admin.php
@@ -11,25 +11,7 @@
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
     integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
     crossorigin=""></script>
-    <script src="leaflet.js" defer>
-      var map = L.map('map').setView([44.4563, 0.1325], 10);
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-
-      L.marker([44.5501, 0.0051]).addTo(map)
-          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-          .openPopup();
-
-      L.circle([44.57873, -0.03416], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 500
-      }).addTo(mymap);
-
-    </script>
     <style>
       #map{ height: 300px;}
     </style>
@@ -54,5 +36,27 @@
     </div>
 
 </body>
+<script defer>
+
+  var divMap = document.getElementById('map')
+  console.log(divMap);
+  var map = L.map(divMap).setView([44.4563, 0.1325], 10);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([44.5501, 0.0051]).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
+  L.circle([44.57873, -0.03416], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+  }).addTo(mymap);
+
+</script>
 
 </html>
