3384b46f11241f8b762bb212c65993d47f7d000b
HugoSab <hsaboural@gmail.com>
2020-05-12 16:02:08 +0200

    Commentaire Login.php

--- Login/login.php
+++ Login/login.php
@@ -1,5 +1,5 @@
 <html>
-<?php require_once '../database/db.php';  ?> <title> Login </title>
+<?php require_once '../database/db.php';  ?> <title> Login </title> <!-- Récupère les éléments de la BDD -->
 <meta charset="utf8">
 <link type="text/css" rel="stylesheet" href="style.css">
 <script type="text/javascript" src="login.js"></script>
@@ -7,31 +7,33 @@
 
 <head>
     <link rel="shortcut icon" type="image/ico" href="index/images/favicon.png"> <!-- favicon -->
-    <title> Login </title>
-    <meta charset="utf8">
-    <link type="text/css" rel="stylesheet" href="style.css">
-    <script type="text/javascript" src="login.js"></script>
+    <title> Login </title> <!-- Titre de la page -->
+    <meta charset="utf8"> <!-- Méta Charset UTF-8 qui permet l'affichage de caractères spéciaux -->
+    <link type="text/css" rel="stylesheet" href="style.css"> <!-- CSS Relié -->
+    <script type="text/javascript" src="login.js"></script> <!-- JS Relié -->
 
 </head>
 
 <body>
 
 
-    <img src="fondcarte.png" class="fondcarte">
-    <div class="loginbox">
-        <img src="loginpic.svg" class="avatar"> <br>
-        <h1> Page Administrateur </h1>
-        <form method="POST">
+    <img src="fondcarte.png" class="fondcarte"> <!-- Image du fonds de carte -->
+    <div class="loginbox"> <!-- DEBUT DE LA DIV LOGINBOX -->
+        <img src="loginpic.svg" class="avatar"> <br>  <!-- Image qui représente un utilisateur -->
+        <h1> Page Administrateur </h1> <!-- Titre : Page Administrateur  -->
+        <form method="POST"> <!-- Méthode POST pour le formulaire -->
+
             <?php
 
             if (!empty($_POST)) {
                 extract($_POST);
                 $valid = true;
-
+                /* Si l'utilisateur ne rentre pas de nom d'utilisateur : Un message apparait : "Veuillez renseigner un nom d'utilisateur! " */ 
                 if (empty($username)) {
                     $valid = false;
                     $error_username = "Veuillez renseigner un nom d'utilisateur !";
                 }
+                /* Si l'utilisateur ne rentre pas de mot de passe : Un message apparait : "Veuillez renseigner un mot de passe! " */ 
                 if (empty($password)) {
                     $valid = false;
                     $error_password = "Veuillez renseigner un mot de passe ! ";
@@ -39,38 +41,48 @@
             }
 
             ?>
-            <p> Nom d'utilisateur </p>
+            <p> Nom d'utilisateur </p> <!-- NOM D'UTILISATEUR -->
+
             <?php
+            
+            /* Ici s'affiche : "Veuillez renseigner un nom d'utilisateur!" */ 
             if (isset($error_username)) {
                 echo "<br> <strong>" . $error_username . "</strong>";
             }
             ?>
+
+            <!-- Champ pour rentrer le nom d'utilisateur -->
             <input type="text" name="username" placeholder="Entrez votre nom d'utilisateur">
 
-            <p> Mot de Passe </p>
-            <span class="eye" onclick="myFunction()">
-                <img src="eye-slash-solid.svg" id="hide1" class=".fa.fa-check-circle">
-                <img src="eye-solid.svg" id="hide2">
+            <p> Mot de Passe </p>  <!-- MOT DE PASSE -->
+
+            <span class="eye" onclick="myFunction()">  <!-- Au clic la fonction JS se met en route / Cette fonction permet de masquer ou non le mot de passe que l'on rentre -->
+                <img src="eye-slash-solid.svg" id="hide1" class=".fa.fa-check-circle">  <!-- Image de l'oeil barré qui masque le mot de passe -->
+                <img src="eye-solid.svg" id="hide2">  <!-- Image de l'oeil -->
             </span>
+
             <?php
+            /* Ici s'affiche : "Veuillez renseigner un mot de passe! " */ 
             if (isset($error_password)) {
                 echo "<br> <strong>" . $error_password . "</strong>";
             }
             ?>
 
+            <!-- Champ pour rentrer le mot de passe -->
             <input type="password" name="password" placeholder="Entrez votre mot de passe" id="myInput">
 
+            <!-- Bouton Retour à l'accueil -->
             <input type="button" name="button" onclick="window.location.href = '../index.php'"
                 value=" Retour à l'accueil" href="../index.html">
 
+            <!-- Bouton Connexion -->
             <input type="submit" name="submit" value="Connexion">
         </form>
-    </div>
+    </div> <!-- FIN DE LA DIV LOGINBOX -->
 
     <?php
 
     ?>
-
 
 </body>
 
