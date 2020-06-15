57c6ed4f3db9556059fc7a692f91acdb9b7a86c9
GuilhemDeMarco <55885506+GuilhemDeMarco@users.noreply.github.com>
2020-06-15 15:21:30 +0200

    règlage d'un bug : viewImage.php ouvrait la mauvaise bdd

--- pages/rubriques/viewImage.php
+++ pages/rubriques/viewImage.php
@@ -1,11 +1,15 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=garonweb-db-new', 'root', '');
+$db = new PDO('mysql:host=localhost;dbname=websitedatabase', 'root', '');
     if(isset($_GET['image_id'])) {
-    $sql = "SELECT Photo,Image_type FROM `marqueurs` WHERE ID_marqueur =" . $_GET['image_id'];
-    $result = $db->prepare($sql);
-    $result -> execute();
-    $data = $result -> fetchAll();
-		header("Content-type: " . $data[0]["Image_type"]);
-        echo $data[0]["Photo"];
-	}
+      $sql = "SELECT Photo,Image_type FROM `marqueurs` WHERE ID_marqueur = :id" ;
+      $result = $db->prepare($sql);
+      $result->bindValue(":id",$_GET['image_id']);
+      $result->execute();
+      $data = $result ->fetch();
+      //var_dump($data);
+      //var_dump($_GET['image_id']);
+
+  		header("Content-type: " . $data["Image_type"]);
+      echo $data["Photo"];
+	  }
 ?>
