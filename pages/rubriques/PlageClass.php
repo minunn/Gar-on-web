a324c51638c4a472b9ad9d6aa31268eb4e6f6d5a
GuilhemDeMarco <55885506+GuilhemDeMarco@users.noreply.github.com>
2020-06-16 14:03:31 +0200

    debut plage

---
+++ pages/rubriques/PlageClass.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ *
+ */
+
+// TODO: IMPORTANT
+// Dans la base de donnée, ce qu'on utilise est un polygone
+// Pour modifier les plages, il faut chercher comment ajouter et modifier des polygones
+class PlageClass
+{
+
+  public function getPlageByNom($nomPlage)
+  {
+
+  }
+
+  public function createNewPlage($newPlage,$files = '')
+  {
+    // code...
+  }
+  public function updatePlage($newPlage,$files = '')
+  {
+
+  }
+  public function deletePlage($id)
+  {
+    // code...
+  }
+}
+
+
+ ?>
