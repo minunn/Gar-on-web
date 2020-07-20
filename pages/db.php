1516cbf4792ee8f5c6505bf55d9ab3fbac7a68e9
minunn <minunn2000@gmail.com>
2020-07-20 14:37:20 +0200

    fix admin

--- pages/db.php
+++ pages/db.php
@@ -24,7 +24,7 @@ if (isset($_POST['submit'])) {
             $_SESSION['username'] = $username;
             $_SESSION['password'] = $password;
             /* Redirige vers la page admin si la connexion s'effectue */ 
-            header('Location: cartographie.php');
+            header('Location: admin.php');
             /* Exit de la fonction */ 
             exit();
         }
